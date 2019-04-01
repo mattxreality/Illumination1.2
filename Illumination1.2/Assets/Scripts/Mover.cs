d76cc3a212bf0309f35d0b90ac1bb363196538ea
Matt Hoffman <mattxreality@gmail.com>
2019-03-31 21:38:08 -0400

    Major improvement to SpeedControl and Mover. Still need to tweak Mover
    Mover currently is too slow at beginning, and increases to rapidly. Need to look for Math exponential options for speed increase.

--- Illumination1.2/Assets/Scripts/Mover.cs
+++ Illumination1.2/Assets/Scripts/Mover.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    public float speed;
-    Rigidbody rigidpody;
+    // todo must normalize projectile speed, currently too slow at beginning, to fast later
+
+    public float speed = 75f;
+    public float speedIncrease = 1f;
+    [SerializeField] float speedNormalizer = .8f;
 
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        // Start() is sufficient as this script controls projectiles that only
+        // live for short periods of time. Each projectile will get the latest
+        // speed info from SpeedControl().
+        speedIncrease = SpeedControl.instance.speedIncrease; // external reference
+        SetProjectileSpeed(speedIncrease);
+       
+    }
+
+    public void SetProjectileSpeed(float speedIncrease)
+    {
+        print("projectile speedIncrease received = " + speedIncrease);
+        GetComponent<Rigidbody>().velocity = transform.forward * speed * speedIncrease *speedNormalizer;
     }
 }
--- Illumination1.2/Assets/Scripts/SpeedControl.cs
+++ Illumination1.2/Assets/Scripts/SpeedControl.cs
@@ -18,65 +18,79 @@ public class SpeedControl : MonoBehaviour
 {
     [SerializeField] ParticleSystem playerContact;
 
-    [SerializeField] float initialSpeed = 10f;
-    [SerializeField] float speedMultiplier = 1f;
+    [SerializeField] float initialSpeed = 10f; // starting speed
+    [SerializeField] float baseSpeedBonus = .1f; // percentage bonus for each gate
+    [SerializeField] float speedMultiplier = 1.2f;
     float speedFactor;
-    public int countOfGatesActivated = 0;
-    private float calculatedSpeed;
+    public float speedIncrease;
+    int gatesActivatedReal;
+    int gatesActivatedMultiplier = 1;
+    float currentSpeed;
 
     [SerializeField] float coolDownValue = 10f; // how long after gate contact before speed decreases
     private float currCoolDownValue; // used for countdown and resetting lights & collision
 
+    public static SpeedControl instance;
+
+    void Awake()
+    {
+        // check if this is the only instance. If not, destroy this instance.
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
-
+        gatesActivatedReal = 0;
+        print("Initial Speed = " + currentSpeed);
+        currentSpeed = initialSpeed;
+        BetterWaypointFollower.instance.routeSpeed = currentSpeed;
     }
 
-    void Update()
-    {
-        SetSpeed();
-    }
-
-    void SpeedFactor() // lazy code, there is a better way I'm sure
-    {
-        if (countOfGatesActivated == 1)
-        { speedFactor = speedMultiplier * 2; } // speed increased after 1 gate
-
-        else if (countOfGatesActivated == 2)
-        { speedFactor = speedMultiplier * 3; } // speed increased after 2 gates
-
-        else if (countOfGatesActivated == 3)
-        { speedFactor = speedMultiplier * 4; } // speed increased after 3 gates
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "accelerator")
         {
             // todo gate is counted twice, or there are two collider interactions
-            countOfGatesActivated++;
+
+            gatesActivatedReal++; // increment count of gates activated by 1
+            gatesActivatedMultiplier++; // increments speed bonus factor by 1
+            speedIncrease = gatesActivatedMultiplier * baseSpeedBonus;
             print("collided with accelerator gate");
-            print("Count of gates activated = " + countOfGatesActivated);
+            print("Count of gates activated = " + gatesActivatedReal);
+            print("Current gateMultiplier = " + gatesActivatedMultiplier);
+            SetSpeed();
 
             // FX when player passes through gate
             Instantiate(playerContact, transform.position, transform.rotation);
 
-            countOfGatesActivated++; // increment count of gates activated by 1
             StartCoroutine(StartCountdown(coolDownValue)); // countdown to reset count of gates
+            
         }
     }
 
     private void SetSpeed()
     {
-        if (countOfGatesActivated < 1)
-            calculatedSpeed = initialSpeed;
+        if (currCoolDownValue < 1) // check if countdown timer is finished
+        {
+            gatesActivatedReal = 1; // reset activated gate count
+        }
+        if (gatesActivatedReal < 1)
+            // todo add lerp to gragually change speed
+            currentSpeed = initialSpeed;
         else
-            calculatedSpeed = countOfGatesActivated * speedMultiplier;
-        
-       
+            currentSpeed = currentSpeed + (initialSpeed * speedIncrease);
+        print("CurrentSpeed = " + currentSpeed);
         // changes speed of BetterWaypointFollower
         // todo, speed reduction currently not working. 
-        BetterWaypointFollower.instance.routeSpeed = calculatedSpeed; 
+        BetterWaypointFollower.instance.routeSpeed = currentSpeed;
     }
 
 
