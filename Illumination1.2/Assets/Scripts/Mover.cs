7e0e7ff4dbfe6358a8f41ef60cf93ff273f279c9
Matt Hoffman <mattxreality@gmail.com>
2019-04-28 19:25:43 -0400

    Major improvements to level design. Refinement of player interaction.
    Added enemy spheres. Great improvement of level design.

--- Illumination1.2/Assets/Scripts/Mover.cs
+++ Illumination1.2/Assets/Scripts/Mover.cs
@@ -6,23 +6,18 @@ public class Mover : MonoBehaviour
 {
     // todo must normalize projectile speed, currently too slow at beginning, to fast later
 
-    public float speed = 100f;
-    public float speedIncrease = 1f;
+    [SerializeField] float speed = 50f;
+    [SerializeField] float speedIncrease = 1f;
     [SerializeField] float speedNormalizer = .7f;
+    //Rigidbody rigidbody;
 
     void Start()
     {
         // Start() is sufficient as this script controls projectiles that only
         // live for short periods of time. Each projectile will get the latest
         // speed info from SpeedControl().
-        speedIncrease = SpeedControl.instance.speedIncrease; // external reference
-        SetProjectileSpeed(speedIncrease);
-       
-    }
+        //speedIncrease = SpeedControl.instance.speedIncrease; // external reference
 
-    public void SetProjectileSpeed(float speedIncrease)
-    {
-        print("projectile speedIncrease received = " + speedIncrease);
-        GetComponent<Rigidbody>().velocity = transform.forward * speed * speedIncrease *speedNormalizer;
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 }
